12b04ecde93dba5597def59b8aeff837d4d05c0a
codydeatherage <cdeatherage14@gmail.com>
2021-07-03 22:48:26 -0700

    now shows correct Rune choices

--- src/pages/MatchListItem.jsx
+++ src/pages/MatchListItem.jsx
@@ -38,6 +38,7 @@ const SpellsContainer = styled.div`
     height: 50px;
     width: 50px;
     border: 1px solid blue;
+    background-color: black;
 `
 const SpellImg = styled.img`
     border: 1px solid green;
@@ -45,8 +46,8 @@ const SpellImg = styled.img`
     width: 47.5%;
 `
 const MatchListItem = ({ match, data, name }) => {
-    const findPlayer = (arr) =>{
-        let p= '';
+    const findPlayer = (arr) => {
+        let p = '';
         for (let d of arr) {
             if (d.summonerName.toLowerCase() === name.toLowerCase()) {
                 p = d;
@@ -54,7 +55,7 @@ const MatchListItem = ({ match, data, name }) => {
         }
         return p;
     }
-    
+
     function msToTime(ms) {
         let seconds = (ms / 1000).toFixed(0);
         let minutes = (ms / (1000 * 60)).toFixed(0);
@@ -86,6 +87,38 @@ const MatchListItem = ({ match, data, name }) => {
         }
     }
 
+    const getRuneName = (runeId) => {
+        switch (runeId) {
+            case 8112: return "Electrocute";
+            case 8124: return "Predator";
+            case 8128: return "DarkHarvest";
+            case 9923: return "HailOfBlades";
+            case 8351: return "GlacialAugment";
+            case 8360: return "Unsealed Spellbook";
+            case 8358: return "MasterKey";//omnistone dev namebreak;
+            case 8005: return "PressTheAttack";
+            case 8008: return "LethalTempo";
+            case 8021: return "FleetFootwork";
+            case 8010: return "Conqueror";
+            case 8437: return "GraspOfTheUndying";
+            case 8439: return "Aftershock";
+            case 8465: return "Guardian";
+            case 8214: return "SummonAery";
+            case 8229: return "ArcaneComet";
+            case 8230: return "PhaseRush";
+        }
+    }
+
+    const getRuneStyle = (runeId) => {
+        switch (runeId) {
+            case 8000: return 'Precision';
+            case 8100: return "Domination";
+            case 8200: return "Sorcery";
+            case 8300: return "Inspiration";
+            case 8400: return "Resolve";
+        }
+    }
+
     if (data) {
         let timeSinceGame = 0;
         let currDate = Date.now();
@@ -93,6 +126,31 @@ const MatchListItem = ({ match, data, name }) => {
         timeSinceGame = msToTime(currDate - data.gameCreation);
         let summoner1 = getSummonerSpellName(player.summoner1Id);
         let summoner2 = getSummonerSpellName(player.summoner2Id);
+        let primaryStyle = getRuneStyle(player.perks.styles[0].style);
+        let primaryRuneName = getRuneName(player.perks.styles[0].selections[0].perk);
+        let secondStyle = getRuneStyle(player.perks.styles[1].style);
+        let primIcon = '';
+        switch (primaryRuneName) {
+            case "Aftershock": primIcon = 'perk-images/Styles/Resolve/VeteranAftershock/VeteranAftershock.png';
+                break;
+            case "LethalTempo": primIcon = 'perk-images/Styles/Precision/LethalTempo/LethalTempoTemp.png'; break;
+            default: primIcon = `perk-images/Styles/${primaryStyle}/${primaryRuneName}/${primaryRuneName}.png`;
+        }
+
+        let primaryRune = { name: primaryRuneName, style: primaryStyle, icon: primIcon }
+
+
+        let secondStyleIcon = '';
+        switch (secondStyle) {
+            case 'Domination': secondStyleIcon = 'perk-images/Styles/7200_Domination.png'; break;
+            case 'Sorcery': secondStyleIcon = 'perk-images/Styles/7202_Sorcery.png'; break;
+            case 'Inspiration': secondStyleIcon = 'perk-images/Styles/7203_Whimsy.png'; break;
+            case 'Resolve': secondStyleIcon = 'perk-images/Styles/7204_Resolve.png'; break;
+            case 'Precision': secondStyleIcon = 'perk-images/Styles/7201_Precision.png'; break;
+        }
+
+        const secRune = { name: secondStyle, icon: secondStyleIcon }
+        let runes = [primaryRune, secRune];
         return (
             <Container>
                 {data ?
@@ -108,9 +166,9 @@ const MatchListItem = ({ match, data, name }) => {
                         </RoundImg>
                         <SpellsContainer>
                             <SpellImg src={`https://ddragon.leagueoflegends.com/cdn/11.13.1/img/spell/${summoner1}.png`}></SpellImg>
-                            <SpellImg></SpellImg>
+                            <SpellImg src={`https://ddragon.leagueoflegends.com/cdn/img/${runes[0].icon}`}></SpellImg>
                             <SpellImg src={`https://ddragon.leagueoflegends.com/cdn/11.13.1/img/spell/${summoner2}.png`}></SpellImg>
-                            <SpellImg></SpellImg>
+                            <SpellImg src={`https://ddragon.leagueoflegends.com/cdn/img/${runes[1].icon}`}></SpellImg>
                         </SpellsContainer>
                         <div>
                             {`${player.kills}/${player.deaths}/${player.assists}`}
